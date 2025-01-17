63f6de04cbe9a4cadc705a9b04abdb772b601e89
Pakin-pan <pakin.pan@student.mahidol.edu>
2025-01-17 12:18:43 +0700

    move from drive

--- majority.c
+++ majority.c
@@ -2,9 +2,16 @@
 
 int main()
 {   
-
-    int n = 10, maj, count = 0;//3
-    int data[n];//1
+    int n = 0;//1
+    int data[500];//1
+    while(1)//depend on how big is the data = n times
+    {
+        scanf("%d",&data[n]);//1
+        if(data[n]==0)//1
+            break;//1
+        n++;//1
+    }
+    int maj, count = 0;//2
 
         //loop through array to find majority  
         //This loop is O(n) because everything inside the loop is O(1)
@@ -31,7 +38,7 @@ int main()
             printf("%d is a majority\n",maj);//1
         else //1
             printf("There is no majority");//1
-
-    return 0;
-    //T1(n)=10n+11
+            
+    return 0;//5
+    //T1(n)=12n+16
 }
